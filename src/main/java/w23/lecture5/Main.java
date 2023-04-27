06106334b9bd504a3ceb6de19f50b9727fc59126
Fred Carella <fred.carella@gmail.com>
2023-04-27 09:42:45 -0400

    commit

---
+++ src/main/java/w23/lecture5/Main.java
@@ -0,0 +1,11 @@
+package w23.lecture5;
+
+public class Main {
+    public static void main(String[] args) {
+//        App app=new App();
+//        app.run();
+
+        new App().run();
+
+    }
+}

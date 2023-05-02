18e3bcb1bac59aabc56d4fab54c4a24fd23d4df2
Fred Carella <fred.carella@gmail.com>
2023-05-02 08:20:34 -0400

    commit

---
+++ src/main/java/w23/lecture7/App.java
@@ -0,0 +1,19 @@
+package w23.lecture7;
+
+public class App {
+    // instance variable
+//    private Scanner in=new Scanner(System.in);
+    public void run() {
+        System.out.println("example 1");
+
+        for(int counter=1;counter<=10;counter++){
+            System.out.println("counter="+counter);
+        }
+
+        for(int counter=1;counter<=10;counter++){
+            System.out.println("counter="+counter);
+        }
+
+
+    }
+}
---
+++ src/main/java/w23/lecture7/Main.java
@@ -0,0 +1,11 @@
+package w23.lecture7;
+
+public class Main {
+    public static void main(String[] args) {
+//        App app=new App();
+//        app.run();
+
+        new App().run();
+
+    }
+}

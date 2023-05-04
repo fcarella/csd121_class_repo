e26cd7e89bdd90d9e7bbc79e567711b43cb6ec55
Fred Carella <fred.carella@gmail.com>
2023-05-04 09:12:19 -0400

    commit

---
+++ src/main/java/w23/lab4_partial_solution/q2/Main.java
@@ -0,0 +1,8 @@
+package w23.lab4_partial_solution.q2;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5,6,7};
+        Table t=new Table(5,nums.length, nums);
+    }
+}
